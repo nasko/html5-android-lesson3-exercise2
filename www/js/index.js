35355cecea0116225de973ac1f5107cae795133f
Atanas Vasilev <atanas.vasilev@gmail.com>
2015-02-19 20:31:01 +0200

    Created animated stickman

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,68 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+var pause = false;
+var xCenter = 75;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+//init function
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("resume", onResume, false);
+}
 
-        console.log('Received Event: ' + id);
+//handle deviceready event
+function onDeviceReady() {
+    // Now it is safe to start the animation
+    window.requestAnimationFrame(draw);
+}
+
+//handle pause event
+function onPause() {
+    pause = true;
+}
+
+//handle resume event
+function onResume() {
+    pause = false;
+    window.requestAnimationFrame(draw);
+}
+
+//Draw the Sun, the Earth and its orbit
+function draw() {
+    if (true === pause) {
+        return;
     }
-};
+    var c = document.getElementById("draw");
+    var ctx = c.getContext("2d");
 
-app.initialize();
+    //Clear the canvas
+    ctx.clearRect(0,0,300,300);
+
+    ctx.beginPath();
+    ctx.arc(xCenter,75,25,0,Math.PI*2,true); // Outer circle
+    ctx.moveTo(xCenter,100);
+    ctx.lineTo(xCenter,175);
+    ctx.lineTo((xCenter - 30),225);
+    ctx.moveTo(xCenter,175);
+    ctx.lineTo((xCenter + 30),225);
+
+    ctx.moveTo(xCenter,130);
+    ctx.lineTo((xCenter + 30),150);
+
+    ctx.moveTo(xCenter,130);
+    ctx.lineTo((xCenter - 30),150);
+
+
+    ctx.stroke();
+
+    //Save the entire default state of the canvas
+    ctx.save();
+
+    //Move the canvas and its origin x horizontally and y vertically
+    //to the center of our drawing area.
+    xCenter += 1;
+
+    //Restore the most recently saved canvas state.
+    ctx.restore();
+
+    //Indirect recursion through callback to draw the next frame
+    window.requestAnimationFrame(draw);
+}
